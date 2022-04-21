889550c04c6980a38c5764f04b5192ee10262c61
vieirapcm-stone <pedro.mvieira@stone.com.br>
2022-04-21 16:11:16 -0300

    Atualizando o código

--- helpers/string_cleaner.py
+++ helpers/string_cleaner.py
@@ -6,8 +6,8 @@ class StringCleaner:
     def remove_url(text):
         return re.sub(r'http\S+', '',text)
 
-    def remove_between_square_brackets(text):
-        return re.sub('\[[^]]*\]', '', text)
-
     def remove_special_characters(text):
-        return unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("ASCII")
+        text = text.replace('-', ' ')
+        text = unicodedata.normalize("NFKD", text).encode("ASCII", "ignore").decode("ASCII")
+        return re.sub('[^a-zA-Z0-9 \\\]', '', text)
+        
